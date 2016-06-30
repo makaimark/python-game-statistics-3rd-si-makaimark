d80cf829107bdb95884f5867c39eefdb810f7a86
makaimark <makaimark@gmail.com>
2016-06-30 21:31:13 +0200

    working

--- part2/export.py
+++ part2/export.py
@@ -3,9 +3,12 @@
 
 import reports
 
+file_name = "game_stat.txt"
+title = "Crysis"
 
-def file_export(file_name, title):
-    with open(file_name, 'w') as f:
+
+def file_export(export_file_name, title):
+    with open(export_file_name, 'w') as f:
         f.write("1. What is the most played game?\n")
         f.write(str(reports.get_most_played(file_name)) + "\n")
         f.write("2. How many copies have been sold total?\n")
@@ -23,3 +26,5 @@ def file_export(file_name, title):
         f.write(str(reports.count_grouped_by_genre(file_name))+'\n')
         f.write("2. What is the date ordered list of the games in descending order?\n")
         f.write(str(reports.get_date_ordered(file_name))+'\n')
+
+file_export("export.txt", title)
--- part2/reports.py
+++ part2/reports.py
@@ -1,11 +1,10 @@
-import export
 from operator import itemgetter
 # Report functions
 
 
-def file_reader():
+def file_reader(file_name):
     opened_text = []
-    with open("game_stat.txt", "r") as f:
+    with open(file_name, "r") as f:
         for line in f.readlines():
             opened_text.append(line.split("\t"))
     return opened_text
@@ -15,7 +14,7 @@ def file_reader():
 def get_most_played(file_name):
     most_played = ""
     number_of_sold = 0
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         if float(line[1]) > float(number_of_sold):
             number_of_sold = float(line[1])
@@ -26,7 +25,7 @@ def get_most_played(file_name):
 # How many copies have been sold total?
 def sum_sold(file_name):
     total_sold = 0
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         total_sold = total_sold + float(line[1])
     return total_sold
@@ -36,8 +35,7 @@ def sum_sold(file_name):
 def get_selling_avg(file_name):
     summa = 0
     counter = 0
-    average = 0
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         counter += 1
         summa = summa + float(line[1])
@@ -48,7 +46,7 @@ def get_selling_avg(file_name):
 # How many characters long is the longest title?
 def count_longest_title(file_name):
     counter = 0
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         if len(line[0]) > counter:
             counter = len(line[0])
@@ -57,10 +55,8 @@ def count_longest_title(file_name):
 
 # What is the average of the release dates?
 def get_date_avg(file_name):
-    counter = 0
-    summa = 0
-    average = 0
-    opened_file = file_reader()
+    summa, average, counter = 0, 0, 0
+    opened_file = file_reader(file_name)
     for line in opened_file:
         counter += 1
         summa = summa + int(line[2])
@@ -71,7 +67,7 @@ def get_date_avg(file_name):
 
 # What properties has a game?
 def get_game(file_name, title):
-        opened_file = file_reader()
+        opened_file = file_reader(file_name)
         for line in opened_file:
             if line[0] == title:
                 line[1] = float(line[1])
@@ -83,7 +79,7 @@ def get_game(file_name, title):
 # How many games are there grouped by genre?
 def count_grouped_by_genre(file_name):
     dictionary = {}
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         line[3] = line[3].replace('\n', "")
         if line[3] not in dictionary.keys():
@@ -97,7 +93,7 @@ def count_grouped_by_genre(file_name):
 def get_date_ordered(file_name):
     temp_dict = {}
     date_ordered_list = []
-    opened_file = file_reader()
+    opened_file = file_reader(file_name)
     for line in opened_file:
         temp_dict[line[0]] = int(line[2])
     temp_dict = sorted(temp_dict.items(), key=lambda x: (-x[1], x[0]))
